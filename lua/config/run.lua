f743ece71d7f32b279430d9fa39fe463c41f1074
sathish <rsathishikumar3@gmail.com>
2026-07-18 23:29:26 +0530

    Some changes

--- lua/config/run.lua
+++ lua/config/run.lua
@@ -11,6 +11,17 @@ local function runPython(file)
   term:toggle()
 end
 
+local function runShell(file)
+  print("Running Shell script...")
+  local term = Terminal:new({
+    cmd = "bash " .. vim.fn.shellescape(file),
+    direction = "float",
+    close_on_exit = false,
+  })
+
+  term:toggle()
+end
+
 vim.keymap.set("n", "<leader>r", function()
   local file = vim.fn.expand("%:p")
   local ext = vim.fn.fnamemodify(file, ":e")
@@ -18,5 +29,8 @@ vim.keymap.set("n", "<leader>r", function()
   if ext == "py" then
     vim.cmd("w")
     runPython(file)
+  elseif ext == "sh" then
+    vim.cmd("w")
+    runShell(file)
   end
 end)
---
+++ lua/plugins/timers.lua
@@ -0,0 +1,6 @@
+return {
+  'ravsii/timers.nvim',
+  version = "*", -- use latest stable release
+  -- See below, empty is fine
+  opts = {},
+}
